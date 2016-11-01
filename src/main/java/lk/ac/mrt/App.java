6c1fbaa3ff413e8d428ca65714618c1438955a5d
chamika <chamikaw@gmail.com>
2016-11-02 01:34:27 +0530

    Initial commit

---
+++ src/main/java/lk/ac/mrt/App.java
@@ -0,0 +1,13 @@
+package lk.ac.mrt;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Distributed File Searcher " );
+    }
+}
